ef08520c4c71e279068e9ae2c3f69d3c49df8189
Julia Labudova <j.labudova@limesoda.com>
2022-02-01 21:40:45 +0100

    EXERCISE-5 Add histogram

---
+++ 04-lection2/01-histogram/histogram.js
@@ -0,0 +1,5 @@
+let statisticItems = document.getElementsByClassName('histogram__statistics-item');
+
+for (let statisticItem of statisticItems) {
+    statisticItem.style.height = Math.floor(Math.random() * 51) + 'px';
+}
