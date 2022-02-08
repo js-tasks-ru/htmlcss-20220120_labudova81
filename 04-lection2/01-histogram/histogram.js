c8faab2845f71a2d0dcbd2692e23a8f091d6e7cc
Julia Labudova <j.labudova@limesoda.com>
2022-02-08 14:27:17 +0100

    EXERCISE-5 Fix comments

--- 04-lection2/01-histogram/histogram.js
+++ 04-lection2/01-histogram/histogram.js
@@ -1,5 +1,5 @@
 let statisticItems = document.getElementsByClassName('histogram__statistics-item');
 
 for (let statisticItem of statisticItems) {
-    statisticItem.style.height = Math.floor(Math.random() * 51) + 'px';
+    statisticItem.style.height = Math.floor(Math.random() * 100) + '%';
 }
